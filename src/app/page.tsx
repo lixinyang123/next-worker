d5dea5641c36c8c89dae9db579fafdbd9f003420
lixinyang123 <lixinyangemil@outlook.com>
2024-12-31 14:00:51 +0800

    add theme toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { ModeToggle } from '@/components/ui/mode-toggle';
 
 export default function Home() {
   return (
@@ -47,6 +48,7 @@ export default function Home() {
           >
             Read our docs
           </a>
+          <ModeToggle />
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
