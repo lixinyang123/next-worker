925b81b9ff57d9ec137742d43613bbcce14e7f86
lixinyang123 <lixinyangemil@outlook.com>
2025-01-01 12:20:00 +0800

    update api

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -6,7 +6,6 @@ export const runtime = 'edge'
 export async function GET(request: NextRequest) {
     // const { env, cf, ctx } = getRequestContext();
 
-    const rse = await fetch('https://kv.conchbrain.club/demo/get?Hi');
-    const text = await rse.text();
-    return new Response(`Hello from ${request.method} ${text}`);
+    const pathname = new URL(request.url).pathname
+    return new Response(`Hello world from ${pathname}`);
 }
